95a85bc52c086d1851fb588a89dc1b2b98a16bf5
Catlyn Bowles <catlynbowles@gmail.com>
2025-04-15 14:55:01 -0700

    add lottie

--- src/app/components/JeopardyBoard.tsx
+++ src/app/components/JeopardyBoard.tsx
@@ -35,49 +35,50 @@ const JeopardyBoard = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto mt-10 grid grid-cols-4 gap-4 text-center">
+    <>
       <div className="text-center text-2xl font-bold mb-6">
         Total Money: <span className="text-green-500">${totalMoney}</span>
       </div>
+      <div className="grid grid-cols-3 text-center">
+        {/* About Me Column */}
+        <Column
+          data={aboutMeData}
+          header="About Me"
+          handleTileClick={handleTileClick}
+          openedTiles={openedTiles}
+        />
 
-      {/* About Me Column */}
-      <Column
-        data={aboutMeData}
-        header="About Me"
-        handleTileClick={handleTileClick}
-        openedTiles={openedTiles}
-      />
+        {/* Projects Column */}
+        <Column
+          data={projectsData}
+          header="Projects"
+          handleTileClick={handleTileClick}
+          openedTiles={openedTiles}
+        />
 
-      {/* Projects Column */}
-      <Column
-        data={projectsData}
-        header="Projects"
-        handleTileClick={handleTileClick}
-        openedTiles={openedTiles}
-      />
+        {/* Contact Column */}
+        <Column
+          data={contactMeData}
+          header="Contact"
+          handleTileClick={handleTileClick}
+          openedTiles={openedTiles}
+        />
 
-      {/* Contact Column */}
-      <Column
-        data={contactMeData}
-        header="Contact"
-        handleTileClick={handleTileClick}
-        openedTiles={openedTiles}
-      />
+        {/* Thank You Modal */}
+        <Modal
+          isOpen={isThankYouModalOpen}
+          onClose={() => setIsThankYouModalOpen(false)}
+          content={"thank you for visiting !"}
+        />
 
-      {/* Thank You Modal */}
-      <Modal
-        isOpen={isThankYouModalOpen}
-        onClose={() => setIsThankYouModalOpen(false)}
-        content={"thank you for visiting !"}
-      />
-
-      {/* Modal for Tile Content */}
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        content={modalContent}
-      />
-    </div>
+        {/* Modal for Tile Content */}
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          content={modalContent}
+        />
+      </div>
+    </>
   );
 };
 
--- src/app/components/Tile.tsx
+++ src/app/components/Tile.tsx
@@ -1,22 +1,32 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import React from "react";
 
-type TileProps = {
+interface TileProps {
   tileKey: string;
   tile: { value: string; detail: string };
   handleTileClick: (detail: string, tileKey: string, value: string) => void;
   isOpened: boolean;
-};
+}
 
 const Tile = ({ tileKey, tile, handleTileClick, isOpened }: TileProps) => {
   return (
     <div
       key={tileKey}
-      className={`bg-blue-900 text-yellow-400 text-lg py-6 border border-white hover:scale-105 transition-transform cursor-pointer ${
-        isOpened ? "bg-gray-500 text-gray-300" : ""
-      }`} // Change style if tile is opened
+      className={`relative w-full h-full cursor-pointer border border-white`}
       onClick={() => handleTileClick(tile.detail, tileKey, tile.value)}
     >
-      {tile.value}
+      {/* Lottie Animation */}
+      <DotLottieReact
+        src="https://lottie.host/ca27c5a0-92b2-4b9b-b4cf-4c859bd32319/K6yO9xyNlm.lottie"
+        loop
+        autoplay
+        className="absolute inset-0 w-full h-full pointer-events-none"
+      />
+
+      {/* Tile Content */}
+      <div className="relative z-10 flex items-center justify-center h-full text-center text-white font-bold">
+        {tile.value}
+      </div>
     </div>
   );
 };
